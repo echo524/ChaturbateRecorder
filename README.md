cf56a78f8350fb893e1cca0d0a339c766c1dad6d
Ethan <75281881+echo524@users.noreply.github.com>
2021-06-03 15:18:57 -0400

    rewrite

--- README.md
+++ README.md
@@ -2,19 +2,32 @@
 
 This is script to automate the recording of public webcam shows from chaturbate.com. 
 
+Tested on Windows 10 - Version 20H2
 
-I have tested this on debian(7+8), ubuntu 14, freenas10 (inside a jail), and Mac OS X (10.10.4), but it should run on other OSs
-I do not have a windows machine to test on, but I had another user test it on windows and has reported the 6/21/17 update as working on windows 10 using python3.6.2  (may also work on python3.5+)
 ## Requirements
 
-Requires python3.5 or newer. You can grab python3.5.2 from https://www.python.org/downloads/release/python-352/
+Requires python3.9 or newer. You can grab python3.9 from https://www.python.org/downloads/
 
 to install required modules, run:
 ```
-python3.5 -m pip install livestreamer bs4 lxml gevent
+python3 -m pip install livestreamer bs4 lxml gevent
 ```
 
+## Usage
+Edit the config file (config.conf) to point to the directory you want to record to, where your "wanted" file is located, 
+which genders, and the interval between checks (in seconds)
 
-Edit the config file (config.conf) to point to the directory you want to record to, where your "wanted" file is located, which genders, and the interval between checks (in seconds)
+Add models to the "wanted.txt" file (only one model per line). 
+The model should match the models name in their chatrooms URL (https://chaturbate.com/{modelname}/). 
+To clarify this, it should only be the "modelname" portion, not the entire url.
 
-Add models to the "wanted.txt" file (only one model per line). The model should match the models name in their chatrooms URL (https://chaturbate.com/{modelname}/). T clarify this, it should only be the "modelname" portion, not the entire url.
+
+## Issues
+I am by no means a python wizard or even a great coder.
+
+If you have contributions or errors, please provide as much info as possible for me to review.
+
+
+### FORK
+This is a fork of  Damianonymous' original script which can be found here: 
+https://github.com/Damianonymous/ChaturbateRecorder
--- getModels.py
+++ getModels.py
@@ -6,63 +6,137 @@ from bs4 import BeautifulSoup
 Config = configparser.ConfigParser()
 Config.read(sys.path[0] + "/config.conf")
 genders = re.sub(' ', '', Config.get('settings', 'genders')).split(",")
-lastPage = {'female': 100, 'couple': 100, 'trans': 100, 'male': 100}
+lastPage = {'female': 10, 'couple': 10, 'trans': 10, 'male': 10}
+
+q = Queue()
+online = []
+workers = []
 
 
-def getOnlineModels():
+green = '\033[32m'
+white = '\033[0m'
+red = '\033[31m'
+
+# def getOnlineModels():
+#     global lastPage
+#     global q
+#     global online
+#     if not q.empty():
+#         args = q.get()
+#         page = args[0]
+#         gender = args[1]
+#         if page < lastPage[gender]:
+#             attempt = 1
+#             while attempt <= 3:
+#                 try:
+#                     print('CheckPoint #1')
+#                     timeout = gevent.Timeout(8)
+#                     timeout.start()
+#                     URL = "https://chaturbate.com/{gender}-cams/?page={page}".format(gender=gender.lower(), page=page)
+#                     result = requests.request('GET', URL)
+#                     result = result.text
+#                     soup = BeautifulSoup(result, 'lxml')
+#                     print(page)
+#                     if lastPage[gender] == 20:
+#                         break
+#                     print(int(soup.findAll('li', {'class': 'active'})[1].string))
+#                     if int(soup.findAll('li', {'class': 'active'})[1].string) == page:
+#                         print('CheckPoint #2')
+#                         print(int(soup.findAll('li', {'class': 'active'})[1].string))
+#                         LIST = soup.findAll('ul', {'class': 'list'})[0]
+#                         models = LIST.find_all('div', {'class': 'title'})
+#                         for model in models:
+#                             online.append(model.find_all('a', href=True)[0].string.lower()[1:])
+#                     break
+#                 except gevent.Timeout:
+#                     print('CheckPoint #3')
+#                     attempt = attempt + 1
+#                     if attempt > 3:
+#                         break
+
+def getOnlineModels2():
     global lastPage
     global q
     global online
-    if not q.empty():
-        args = q.get()
-        page = args[0]
-        gender = args[1]
-        if page < lastPage[gender]:
+    for gender in genders:
+        page = 1
+        lastP = 0
+        whileCount = 0
+
+        # only go through pages upto last page
+        while page < lastPage[gender]:
             attempt = 1
-            while attempt <= 3:
+            whileCount += 1
+            #print('While Count: ' + str(whileCount))
+
+            # if the page is not the same as last
+            if lastP != page:
                 try:
+                    #print('CheckPoint #1')
                     timeout = gevent.Timeout(8)
                     timeout.start()
                     URL = "https://chaturbate.com/{gender}-cams/?page={page}".format(gender=gender.lower(), page=page)
                     result = requests.request('GET', URL)
                     result = result.text
                     soup = BeautifulSoup(result, 'lxml')
-                    if lastPage[gender] == 100:
-                        lastPage[gender] = int(soup.findAll('a', {'class': 'endless_page_link'})[-2].string)
+
+                    #print('Page: ' + str(page))
+                    #print('Last Page: ' + str(lastP))
+                    #if lastPage[gender] == 20:
+                        #break
+                    #print('Test: ' + str(soup.findAll('li', {'class': 'active'})))
+
+                    #print(green + str(int(soup.findAll('li', {'class': 'active'})[1].string)) + white)
+                    lastP = page
                     if int(soup.findAll('li', {'class': 'active'})[1].string) == page:
+                        #print('CheckPoint #2')
                         LIST = soup.findAll('ul', {'class': 'list'})[0]
                         models = LIST.find_all('div', {'class': 'title'})
                         for model in models:
                             online.append(model.find_all('a', href=True)[0].string.lower()[1:])
-                    break
+                        page += 1
+                        #print(red + 'Page After: ' + str(page) + white)
+
+
                 except gevent.Timeout:
+                    #print('CheckPoint #3')
                     attempt = attempt + 1
                     if attempt > 3:
                         break
+            elif lastP == page:
+                attempt += 1
 
 
-def getModels():
-    workers = []
-    for gender in genders:
-        if gender == 'couple':
-            for i in range(1, 3):
-                q.put([i, gender])
-        else:
-            for i in range(1, 30):
-                q.put([i, gender])
-    while not q.empty():
-        for i in range(10):
-            t = Thread(target=getOnlineModels)
-            workers.append(t)
-            t.start()
-        for t in workers:
-            t.join()
+# def getModels():
+#     global workers
+#     for gender in genders:
+#         if gender == 'couple':
+#             for i in range(1, 3):
+#                 q.put([i, gender])
+#         else:
+#             for i in range(1, 30):
+#                 q.put([i, gender])
+#     while not q.empty():
+#         for i in range(10):
+#             t = Thread(target=getOnlineModels)
+#             workers.append(t)
+#             t.start()
+#         for t in workers:
+#             t.join()
 
-if __name__ == '__main__':
-    q = Queue()
-    online = []
-    workers = []
-    getModels()
+
+# if __name__ == '__main__':
+#     q = Queue()
+#     online = []
+#     workers = []
+#     getModels()
+#     online = list(set(online))
+#     #for model in online:
+#         #print(model)
+# 		return online
+
+def getMs():
+    global online
+    getOnlineModels2()
     online = list(set(online))
-    for model in online:
-        print(model)
+    return online
